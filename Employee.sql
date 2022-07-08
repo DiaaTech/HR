43823d4b0ed25e9e8c6c444f29fcb1e2cbeab6b6
DiaaTech <101534045+DiaaTech@users.noreply.github.com>
2022-07-07 23:20:12 -0400

    Update Employee.sql

--- Employee.sql
+++ Employee.sql
@@ -2,9 +2,11 @@ REATE DATABASE EMPLOYEE;
 CREATE TABLE EMPLOYEE
 (
     EMPLOYEE_ID INT NOT NULL,
+    -- You forgot to add a PRIMARY KEY--
     FIRST_NAME VARCHAR(50) NOT NULL,
     LAST_NAME VARCHAR(50) NOT NULL,
     DOB DATE,
     SALARY DECIMAL(4,2),
     EMPLOYEE_NAME INT NOT NULL,
+    -- Why you added this here. Please read the assignment question to get the column names--
 )

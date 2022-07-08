7fa4f47eb7490319bc4c2e242a0092ceaa1c32c4
DiaaTech <101534045+DiaaTech@users.noreply.github.com>
2022-07-07 23:10:38 -0400

    Update department-table.sql

--- department-table.sql
+++ department-table.sql
@@ -1,5 +1,6 @@
 create table department (
     department_id INT not null,
+    -- you forgot a primary key for this table--
     department_name VARCHAR NOT NULL,
     number_of_employees INT NOT NULL,
     

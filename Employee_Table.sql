5804b549043532d405bb09dbc7503eaae4f9eadf
DiaaTech <101534045+DiaaTech@users.noreply.github.com>
2022-07-07 23:13:49 -0400

    Update Employee_Table.sql

--- Employee_Table.sql
+++ Employee_Table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE Employee
 (
     Emp_id INT IDENTITY (1,1) NOT NULL,
+    -- You forgot to add a primary key for this table--
     first_name VARCHAR (20) NOT NULL,
     last_name VARCHAR (20) NOT NULL,
     DOB DATE NOT NULL,

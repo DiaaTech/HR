64df9fb85fe59081d414554d08cf2bb43c8e8163
Seenaa <fitasena@gmail.com>
2022-07-07 20:17:42 -0400

    Created Employee Table

---
+++ Employee_Table.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Employee
+(
+    Emp_id INT IDENTITY (1,1) NOT NULL,
+    first_name VARCHAR (20) NOT NULL,
+    last_name VARCHAR (20) NOT NULL,
+    DOB DATE NOT NULL,
+    salary DECIMAL (8,2),
+    department_id INT NOT NULL,
+)

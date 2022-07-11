ed9e44f24f0b941aa5e85d0f7750022c40e71556
your-name-herekhaled anwar <your-email-herekhaledabdu01@gmail.com>
2022-07-11 00:50:56 -0500

    modified the table and make changes

--- CREATE.sql
+++ CREATE.sql
@@ -6,19 +6,23 @@ last_name VARCHAR(50),
 phone_number INT,
 email VARCHAR(50),
 job_id INT FOREIGN KEY REFERENCES jobs(job_id),
-department_id INT 
-)
+department_id INT
+) 
 
-DROP TABLE IF EXISTS  jobs
-CREATE table jobs(
+
+
+DROP TABLE IF EXISTS jobs
+CREATE table jobs (
 job_id INT PRIMARY KEY ,
 job_title VARCHAR(25) NOT NULL,
 min_salary DECIMAL(7,2),
 max_salary DECIMAL(7,2),
-
 )
+
+
 DROP TABLE IF EXISTS department
 CREATE table department(
 department_id INT PRIMARY KEY ,
 department_name vARCHAR(25) NOT NULL,
 )
+

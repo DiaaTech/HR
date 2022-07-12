b9b65d312c6feab1a346d0d76f566c3865ddfbc4
Abdugugu <107594140+Abdugugu@users.noreply.github.com>
2022-07-11 23:47:51 -0400

    HRdatabase

--- department.sql
+++ department.sql
@@ -3,5 +3,4 @@ CREATE table departments
  department_id int primary key not null,
  department_name int,
 number_of_employees varchar(100)
-
 )
--- jobs.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE JOBS
-(
-    job_id int primary key NOT NULL,
-    job_title int,
-    MIN_SALARY float NOT NULL,
-    MAX_SALARY float NOT NULL
-)
